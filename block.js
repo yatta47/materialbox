977c6a70ad867da13c8effdfa7690c009d3ccbe6
yatta47 <yatta47@gmail.com>
2019-08-04 12:42:10 +0900

    initial commit

---
+++ block.js
@@ -0,0 +1,14 @@
+var text_type = new Array(
+    '<a href="https://px.a8.net/svt/ejp?a8mat=35LK2O+91OCEA+3JWG+HY06A" target="_blank" rel="nofollow">参加者限定特典あり。TECH::CAMPオンライン説明会</a><img border="0" width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=35LK2O+91OCEA+3JWG+HY06A" alt="" /> '
+);
+
+var banner_300x250 = new Array(
+    '<a href="https://px.a8.net/svt/ejp?a8mat=35LK2O+91OCEA+3JWG+HWPVL" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=190802400547&wid=011&eno=01&mid=s00000016576003008000&mc=1"></a><img border="0" width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=35LK2O+91OCEA+3JWG+HWPVL" alt="">',
+    '<a href="https://px.a8.net/svt/ejp?a8mat=35JQA8+75FR6+4AU6+61Z81" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www21.a8.net/svt/bgt?aid=190717136012&wid=011&eno=01&mid=s00000020067001017000&mc=1"></a><img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=35JQA8+75FR6+4AU6+61Z81" alt="">',
+    '<a href="https://px.a8.net/svt/ejp?a8mat=35JSMO+A4YYBM+389A+67RK1" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www26.a8.net/svt/bgt?aid=190720176613&wid=011&eno=01&mid=s00000015067001044000&mc=1"></a><img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=35JSMO+A4YYBM+389A+67RK1" alt="">',
+    '<a href="https://px.a8.net/svt/ejp?a8mat=35JQA8+BBGZM+4B0A+60WN5" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=190717136019&wid=011&eno=01&mid=s00000020089001012000&mc=1"></a><img border="0" width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=35JQA8+BBGZM+4B0A+60WN5" alt="">',
+);
+
+var num = Math.floor(Math.random() * banner_300x250.length)
+document.write('<small>スポンサーリンク</small><br>')
+document.write(banner_300x250[num]);
