932fc58484af498687edd0c67c39b5783fe46226
yatta47 <yatta47@gmail.com>
2019-08-04 14:26:03 +0900

    追加

--- block.js
+++ block.js
@@ -2,13 +2,38 @@ var text_type = new Array(
     '<a href="https://px.a8.net/svt/ejp?a8mat=35LK2O+91OCEA+3JWG+HY06A" target="_blank" rel="nofollow">参加者限定特典あり。TECH::CAMPオンライン説明会</a><img border="0" width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=35LK2O+91OCEA+3JWG+HY06A" alt="" /> '
 );
 
-var banner_300x250 = new Array(
-    '<a href="https://px.a8.net/svt/ejp?a8mat=35LK2O+91OCEA+3JWG+HWPVL" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=190802400547&wid=011&eno=01&mid=s00000016576003008000&mc=1"></a><img border="0" width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=35LK2O+91OCEA+3JWG+HWPVL" alt="">',
-    '<a href="https://px.a8.net/svt/ejp?a8mat=35JQA8+75FR6+4AU6+61Z81" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www21.a8.net/svt/bgt?aid=190717136012&wid=011&eno=01&mid=s00000020067001017000&mc=1"></a><img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=35JQA8+75FR6+4AU6+61Z81" alt="">',
-    '<a href="https://px.a8.net/svt/ejp?a8mat=35JSMO+A4YYBM+389A+67RK1" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www26.a8.net/svt/bgt?aid=190720176613&wid=011&eno=01&mid=s00000015067001044000&mc=1"></a><img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=35JSMO+A4YYBM+389A+67RK1" alt="">',
-    '<a href="https://px.a8.net/svt/ejp?a8mat=35JQA8+BBGZM+4B0A+60WN5" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=190717136019&wid=011&eno=01&mid=s00000020089001012000&mc=1"></a><img border="0" width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=35JQA8+BBGZM+4B0A+60WN5" alt="">',
-);
+var banner_300x250 = [
+    {
+        name: 'TECH::CAMP',
+        code: '<a href="https://px.a8.net/svt/ejp?a8mat=35LK2O+91OCEA+3JWG+HWPVL" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www22.a8.net/svt/bgt?aid=190802400547&wid=011&eno=01&mid=s00000016576003008000&mc=1"></a><img border="0" width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=35LK2O+91OCEA+3JWG+HWPVL" alt="">',
+    },
+    {
+        name: 'サポーターズ',
+        code: '<a href="https://px.a8.net/svt/ejp?a8mat=35JQA8+75FR6+4AU6+61Z81" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www21.a8.net/svt/bgt?aid=190717136012&wid=011&eno=01&mid=s00000020067001017000&mc=1"></a><img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=35JQA8+75FR6+4AU6+61Z81" alt="">',
+    },
+    {
+        name: 'ちょびリッチ。',
+        code: '<a href="https://px.a8.net/svt/ejp?a8mat=35JSMO+A4YYBM+389A+67RK1" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www26.a8.net/svt/bgt?aid=190720176613&wid=011&eno=01&mid=s00000015067001044000&mc=1"></a><img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=35JSMO+A4YYBM+389A+67RK1" alt="">',
+    },
+    {
+        name: 'techcareer',
+        code: '<a href="https://px.a8.net/svt/ejp?a8mat=35JQA8+BBGZM+4B0A+60WN5" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=190717136019&wid=011&eno=01&mid=s00000020089001012000&mc=1"></a><img border="0" width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=35JQA8+BBGZM+4B0A+60WN5" alt="">',
+    },
+    {
+        name: '楽天モバイル',
+        code: '<a href="https://px.a8.net/svt/ejp?a8mat=35DJ1G+FRZRLU+399O+64C3L" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www25.a8.net/svt/bgt?aid=190427812954&wid=011&eno=01&mid=s00000015198001028000&mc=1"></a><img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=35DJ1G+FRZRLU+399O+64C3L" alt="">',
+    },
+    {
+        name: 'CodeCamp',
+        code: '<a href="https://px.a8.net/svt/ejp?a8mat=2Z8KU0+98TJNM+3GOM+60H7L" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=180119160559&wid=011&eno=01&mid=s00000016159001010000&mc=1"></a><img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=2Z8KU0+98TJNM+3GOM+60H7L" alt="">',
+    },
+    {
+        name: 'TECHNO WAVE',
+        code: '<a href="https://px.a8.net/svt/ejp?a8mat=2Z8KU0+94NIF6+3SQI+5ZMCH" target="_blank" rel="nofollow"><img border="0" width="300" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=180119160552&wid=011&eno=01&mid=s00000017721001006000&mc=1"></a><img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=2Z8KU0+94NIF6+3SQI+5ZMCH" alt="">',
+    },
+];
 
 var num = Math.floor(Math.random() * banner_300x250.length)
 document.write('<small>スポンサーリンク</small><br>')
-document.write(banner_300x250[num]);
+document.write(banner_300x250[num].code);
+
